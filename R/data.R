a48c1850e67049a7901a2d80e05d0c50ed8fb322
danStich <daniel.stich@oneonta.edu>
2021-08-20 09:34:13 -0400

    initial data docs

---
+++ R/data.R
@@ -0,0 +1,283 @@
+#' @title Downstream data for Browns Mills
+#' 
+#' @description Alden research estimates of downstream survival from the 
+#' Atlantic salmon Dam Impact analysis model v67. Estimates are based on 
+#' in-river flow. 
+#' 
+#' @format A dataframe with 857 observations of 4 variables
+#' \describe{
+#'   \code{flow_cfs } Value of flow (discharge) \cr \cr
+#'   \code{cumulative_prob } Cumulative flow probability \cr \cr
+#'   \code{flow_prob } Probability of individual flow value \cr \cr
+#'   \code{smolt_surv_prob } Smolt survival probability given flow \cr \cr
+#' }
+#' 
+#' @source Nieland et al. (2013, 2015)
+#' 
+"downstream_brownsmills"
+
+#' @title Downstream data for Great Works
+#' 
+#' @description Alden research estimates of downstream survival from the 
+#' Atlantic salmon Dam Impact analysis model v67. Estimates are based on 
+#' in-river flow. 
+#' 
+#' @format A dataframe with 7,168 observations of 4 variables
+#' \describe{
+#'   \code{flow_cfs } Value of flow (discharge) \cr \cr
+#'   \code{cumulative_prob } Cumulative flow probability \cr \cr
+#'   \code{flow_prob } Probability of individual flow value \cr \cr
+#'   \code{smolt_surv_prob } Smolt survival probability given flow \cr \cr
+#' }
+#' 
+#' @source Nieland et al. (2013, 2015)
+#' 
+"downstream_greatworks"
+
+#' @title Downstream data for Howland
+#' 
+#' @description Alden research estimates of downstream survival from the 
+#' Atlantic salmon Dam Impact analysis model v67. Estimates are based on 
+#' in-river flow. 
+#' 
+#' @format A dataframe with 3,315 observations of 4 variables
+#' \describe{
+#'   \code{flow_cfs } Value of flow (discharge) \cr \cr
+#'   \code{cumulative_prob } Cumulative flow probability \cr \cr
+#'   \code{flow_prob } Probability of individual flow value \cr \cr
+#'   \code{smolt_surv_prob } Smolt survival probability given flow \cr \cr
+#' }
+#' 
+#' @source Nieland et al. (2013, 2015)
+#' 
+"downstream_howland"
+
+#' @title Downstream data for Mattaceunk
+#' 
+#' @description Alden research estimates of downstream survival from the 
+#' Atlantic salmon Dam Impact analysis model v67. Estimates are based on 
+#' in-river flow. 
+#' 
+#' @format A dataframe with 8,681 observations of 4 variables
+#' \describe{
+#'   \code{flow_cfs } Value of flow (discharge) \cr \cr
+#'   \code{cumulative_prob } Cumulative flow probability \cr \cr
+#'   \code{flow_prob } Probability of individual flow value \cr \cr
+#'   \code{smolt_surv_prob } Smolt survival probability given flow \cr \cr
+#' }
+#' 
+#' @source Nieland et al. (2013, 2015)
+#' 
+"downstream_mattaceunk"
+
+#' @title Downstream data for Medway
+#' 
+#' @description Alden research estimates of downstream survival from the 
+#' Atlantic salmon Dam Impact analysis model v67. Estimates are based on 
+#' in-river flow. 
+#' 
+#' @format A dataframe with 7,589 observations of 4 variables
+#' \describe{
+#'   \code{flow_cfs } Value of flow (discharge) \cr \cr
+#'   \code{cumulative_prob } Cumulative flow probability \cr \cr
+#'   \code{flow_prob } Probability of individual flow value \cr \cr
+#'   \code{smolt_surv_prob } Smolt survival probability given flow \cr \cr
+#' }
+#' 
+#' @source Nieland et al. (2013, 2015)
+#' 
+"downstream_medway"
+
+#' @title Downstream data for Milford
+#' 
+#' @description Alden research estimates of downstream survival from the 
+#' Atlantic salmon Dam Impact analysis model v67. Estimates are based on 
+#' in-river flow. This dataset was updated in 2020 following changes to the
+#' hydrosystem associated with the Penobscot River Restoration Project.
+#' 
+#' @format A dataframe with 9,356 observations of 4 variables
+#' \describe{
+#'   \code{flow_cfs } Value of flow (discharge) \cr \cr
+#'   \code{cumulative_prob } Cumulative flow probability \cr \cr
+#'   \code{flow_prob } Probability of individual flow value \cr \cr
+#'   \code{smolt_surv_prob } Smolt survival probability given flow \cr \cr
+#' }
+#' 
+#' @source Nieland et al. (2020)
+#' 
+"downstream_milford"
+
+#' @title Downstream data for Moosehead
+#' 
+#' @description Alden research estimates of downstream survival from the 
+#' Atlantic salmon Dam Impact analysis model v67. Estimates are based on 
+#' in-river flow.
+#' 
+#' @format A dataframe with 753 observations of 4 variables
+#' \describe{
+#'   \code{flow_cfs } Value of flow (discharge) \cr \cr
+#'   \code{cumulative_prob } Cumulative flow probability \cr \cr
+#'   \code{flow_prob } Probability of individual flow value \cr \cr
+#'   \code{smolt_surv_prob } Smolt survival probability given flow \cr \cr
+#' }
+#' 
+#' @source Nieland et al. (2013, 2015)
+#' 
+"downstream_moosehead"
+
+#' @title Downstream data for Orono
+#' 
+#' @description Alden research estimates of downstream survival from the 
+#' Atlantic salmon Dam Impact analysis model v67. Estimates are based on 
+#' in-river flow. This dataset was updated in 2020 following changes to the
+#' hydrosystem associated with the Penobscot River Restoration Project.
+#' 
+#' @format A dataframe with 4,457 observations of 4 variables
+#' \describe{
+#'   \code{flow_cfs } Value of flow (discharge) \cr \cr
+#'   \code{cumulative_prob } Cumulative flow probability \cr \cr
+#'   \code{flow_prob } Probability of individual flow value \cr \cr
+#'   \code{smolt_surv_prob } Smolt survival probability given flow \cr \cr
+#' }
+#' 
+#' @source Nieland et al. (2020)
+#' 
+"downstream_orono"
+
+#' @title Downstream data for Stillwater
+#' 
+#' @description Alden research estimates of downstream survival from the 
+#' Atlantic salmon Dam Impact analysis model v67. Estimates are based on 
+#' in-river flow. This dataset was updated in 2020 following changes to the
+#' hydrosystem associated with the Penobscot River Restoration Project.
+#' 
+#' @format A dataframe with 4,456 observations of 4 variables
+#' \describe{
+#'   \code{flow_cfs } Value of flow (discharge) \cr \cr
+#'   \code{cumulative_prob } Cumulative flow probability \cr \cr
+#'   \code{flow_prob } Probability of individual flow value \cr \cr
+#'   \code{smolt_surv_prob } Smolt survival probability given flow \cr \cr
+#' }
+#' 
+#' @source Nieland et al. (2020)
+#' 
+"downstream_stillwater"
+
+#' @title Downstream data for Veazie
+#' 
+#' @description Alden research estimates of downstream survival from the 
+#' Atlantic salmon Dam Impact analysis model v67. Estimates are based on 
+#' in-river flow.
+#' 
+#' @format A dataframe with 13,939 observations of 4 variables
+#' \describe{
+#'   \code{flow_cfs } Value of flow (discharge) \cr \cr
+#'   \code{cumulative_prob } Cumulative flow probability \cr \cr
+#'   \code{flow_prob } Probability of individual flow value \cr \cr
+#'   \code{smolt_surv_prob } Smolt survival probability given flow \cr \cr
+#' }
+#' 
+#' @source Nieland et al. (2013, 2015)
+#' 
+"downstream_veazie"
+
+#' @title Downstream data for West Enfield
+#' 
+#' @description Alden research estimates of downstream survival from the 
+#' Atlantic salmon Dam Impact analysis model v67. Estimates are based on 
+#' in-river flow.
+#' 
+#' @format A dataframe with 9,368 observations of 4 variables
+#' \describe{
+#'   \code{flow_cfs } Value of flow (discharge) \cr \cr
+#'   \code{cumulative_prob } Cumulative flow probability \cr \cr
+#'   \code{flow_prob } Probability of individual flow value \cr \cr
+#'   \code{smolt_surv_prob } Smolt survival probability given flow \cr \cr
+#' }
+#' 
+#' @source Nieland et al. (2013, 2015)
+#' 
+"downstream_westenfield"
+
+#' @title Empirical cumulative distribution function for natural mortality
+#' per kilometer
+#' 
+#' @description Empirical cumulative distribution function for natural mortality
+#' per kilometer derived by Stevens et al. (2019).
+#' 
+#' @format A dataframe with 34 observations of 3 variables
+#' \describe{
+#'   \code{prop_lost_per_km } Proportion of fish lost per kilometer \cr \cr
+#'   \code{cumulative frequency } Cumulative probability at observation \cr \cr
+#'   \code{prob } Probability of occurrence for a given observation \cr \cr
+#' }
+#' 
+#' @source
+#' Stevens, JR, JF Kocik, and TF Sheehan. 2019. Modeling the impacts of dams and 
+#' stocking practices on an endangered Atlantic salmon (Salmo salar) 
+#' population in the Penobscot River, Maine, USA. Canadian Journal of Fisheries
+#' and Aquatic Sciences 76:1795-1807.
+#' 
+"mort_per_km"
+
+#' @title Flow ratios data set
+#' 
+#' @description GageID and associated flow ratios by dam in the Penobscot
+#' River based on Stevens et al. (2019).
+#' 
+#' @format A dataframe with 21 observations of 3 variables
+#' \describe{
+#'   \code{dam } Name of dam \cr \cr
+#'   \code{GageID } USGS Gage used to determine dam-specific flow
+#'   \code{flow_ratio } Ratio of flow from gage \cr \cr
+#' }
+#' 
+#' @source
+#' Stevens, JR, JF Kocik, and TF Sheehan. 2019. Modeling the impacts of dams and 
+#' stocking practices on an endangered Atlantic salmon (Salmo salar) 
+#' population in the Penobscot River, Maine, USA. Canadian Journal of Fisheries
+#' and Aquatic Sciences 76:1795-1807.
+#' 
+"flow_ratios"
+
+#' @title Stillwater logistic regression parameters
+#' 
+#' @description Parameter estimates and statistics for logistic regression
+#' characterizing probability of fish using Stillwater Branch for downstream
+#' migration around Marsh Island in the Penobscot River, USA. Used to predict
+#' probability of fish using Stillwater Branch conditional on flow (cfs).
+#' 
+#' @format A dataframe with 2 observations of 5 variables
+#' \describe{
+#'   \code{parameter } Model parameter \cr \cr
+#'   \code{estimate } Estimated mean for parameter
+#'   \code{se } Estimated standard error for parameter \cr \cr
+#'   \code{z test statistic for regression}
+#'   \code{p p-values for parameters}
+#' }
+#' 
+#' @source
+#' Stich et al. (2014)
+#' 
+"stillwater_pars"
+
+#' @title Stocking data for Penobscot River watershed
+#' 
+#' @description Number of Atlantic salmon smolts stocked in each of 22 sites
+#' by year.
+#' 
+#' @format A dataframe with 1239 observations of 3 variables
+#' \describe{
+#'   \code{Year } Year of stocking \cr \cr
+#'   \code{site_code } Site code corresponding to `stocking_site` in `distance_traveled`
+#'   \code{n_stocked } Number of smolts stocked
+#' }
+#' 
+#' @source
+#' @source
+#' Stevens, JR, JF Kocik, and TF Sheehan. 2019. Modeling the impacts of dams and 
+#' stocking practices on an endangered Atlantic salmon (Salmo salar) 
+#' population in the Penobscot River, Maine, USA. Canadian Journal of Fisheries
+#' and Aquatic Sciences 76:1795-1807.
+#' 
+"stocking_data"
